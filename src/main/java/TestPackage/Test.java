16bd6823c3deade1f41182ae96f59172a745165d
Mikołaj <Mikoaj@MIKOLAJ_PC>
2020-03-19 12:56:47 +0100

    First commit on Test

---
+++ src/main/java/TestPackage/Test.java
@@ -0,0 +1,9 @@
+package TestPackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Testowe dziłanie programu");
+	}
+
+}
---
+++ src/test/java/TestPackage/TestTest.java
@@ -0,0 +1,14 @@
+package TestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		assertTrue("Powinna być prawda", true);
+	}
+
+}
